35923def118f1df3147bfe3b8330bc44df92edf4
23054nk <23054nk@hvhs.school.nz>
2026-03-09 13:28:51 +1300

    save

--- index.js
+++ index.js
@@ -9,38 +9,49 @@
 
 function preload() {
 //image of dinosaur
-img = loadImage('../assets/images/dino.png');
+dinoOneImage = loadImage('assets/images/dino.png');
+personOneFrontImg = loadImage('assets/images/person1front.png');
+personOneSideImg = loadImage('assets/images/person1side.png');
 }
 
 
 function setup() {
-
 	console.log("setup:");
 	cnv = new Canvas(windowWidth,windowHeight);
-	// Dinosaur sprite
-	//dinoOne = new Sprite(200,500,200,100);
-	dinoOne.image=(dino.png);
-	//dinoOne.color ='#808000';
-	dinoOne.bounciness = 1;
+	world.gravity.y = 10;
+	ground = new Sprite(width/2, 900, width, 200, 'static') // 'static' ground does not move due to gravity
+	ground.color = '#839b5d';
+
+
+	// creating dinoOne sprite and attaching image
+	dinoOneSprite = new Sprite(200,500,200,100); 
+	dinoOneSprite.scale = 0.2;
+	dinoOneSprite.img = dinoOneImage; 
+	
+	
+	// creating personOneSprite sprite and attaching image
+	personOneSprite = new Sprite(500,300,200,100);
+	personOneSprite.img = personOneFrontImg; 
+	personOneSprite.scale = 0.2;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*******************************************************/
 function draw() {
-	background('#D3D3D3');
+	background('#c8f2ff');
+
+	// if right arrow button pushed, move player right & switch sprite's image to the side view
+	if (kb.pressing('right')) {
+		personOneSprite.x+=5;
+		personOneSprite.img = personOneSideImg;
+	}
+
+	// if right arrow button isn't being pushed switch back to front view
+	else {
+		personOneSprite.img = personOneFrontImg;
+	}
+
 }
 
 /*******************************************************/
